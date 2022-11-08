ebb179619443961578b106b2322a0a51e5d6c8ea
HypnoGaga <bobawawrk@gmail.com>
2022-11-08 17:27:59 +0200

    RealEstate

---
+++ RealEstate/js/script.js
@@ -0,0 +1,9 @@
+const swiper = new Swiper('.slider-body-main', {
+	// Optional parameters
+	loop: true,
+	// Navigation arrows
+	navigation: {
+		nextEl: '.slider-body-main__arrow.swiper-button-next',
+		prevEl: '.slider-body-main__arrow.swiper-button-prev',
+	},
+});
