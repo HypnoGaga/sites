6362ba496380dd290db6729eb8cfa9b57397318b
HypnoGaga <bobawawrk@gmail.com>
2023-01-03 21:00:20 +0200

    Miami

--- Miami/js/app.js
+++ Miami/js/app.js
@@ -226,14 +226,12 @@
             script_bodyUnlock();
         }
     }
-    window.addEventListener("load", (function() {
-        const appHeight = () => {
-            const doc = document.documentElement;
-            doc.style.setProperty("--app-height", `${window.innerHeight}px`);
-        };
-        window.addEventListener("resize", appHeight);
-        appHeight();
-    }));
+    const appHeight = () => {
+        const doc = document.documentElement;
+        doc.style.setProperty("--app-height", `${window.innerHeight}px`);
+    };
+    window.addEventListener("resize", appHeight);
+    appHeight();
     window["FLS"] = true;
     isWebp();
     addLoadedClass();
