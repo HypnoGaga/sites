4595743c67a880fa5048e628af4b913ca7e2838b
HypnoGaga <bobawawrk@gmail.com>
2023-01-03 11:04:29 +0200

    Miami

---
+++ Miami/js/app.js
@@ -0,0 +1,32 @@
+(() => {
+    "use strict";
+    function isWebp() {
+        function testWebP(callback) {
+            let webP = new Image;
+            webP.onload = webP.onerror = function() {
+                callback(2 == webP.height);
+            };
+            webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+        }
+        testWebP((function(support) {
+            let className = true === support ? "webp" : "no-webp";
+            document.documentElement.classList.add(className);
+        }));
+    }
+    let addWindowScrollEvent = false;
+    setTimeout((() => {
+        if (addWindowScrollEvent) {
+            let windowScroll = new Event("windowScroll");
+            window.addEventListener("scroll", (function(e) {
+                document.dispatchEvent(windowScroll);
+            }));
+        }
+    }), 0);
+    const menuIcon = document.querySelector(".icon-menu");
+    const headerEl = document.querySelector(".header");
+    menuIcon.addEventListener("click", (e => {
+        headerEl.classList.toggle("menu-open");
+    }));
+    window["FLS"] = true;
+    isWebp();
+})();
